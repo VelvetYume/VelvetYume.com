9dfd7b0071d2ac8a2112e1114febbdae6030ae1c
VelvetYume <velvetyumeclohub@gmail.com>
2026-01-11 01:07:46 +0300

    Reduce background images for better mobile performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,24 +31,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const bgImages = [
-    '/images/bg1.jpg',
-    '/images/bg2.jpg',
     '/images/bg3.jpg',
     '/images/bg4.jpg',
-    '/images/bg5.jpg',
     '/images/bg6.jpg',
-    '/images/bg7.jpg',
-    '/images/bg8.jpg',
-    '/images/bg9.jpg',
-    '/images/bg10.jpg',
     '/images/bg11.jpg',
-    '/images/bg12.jpg',
-    '/images/bg16.jpg',
     '/images/bg18.jpg',
-    '/images/bg19.jpg',
-    '/images/bg22.jpg',
-    '/images/bg33.jpg',
-    '/images/bg40.jpg',
   ];
 
   return (
